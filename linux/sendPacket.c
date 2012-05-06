5d50ad3e0045871ca04d05313cc3f6439f307cd9
Adam Eggum <aeggum12@gmail.com>
2012-05-06 02:31:45 -0500

    commented and cleaned up sendPacket a little bit

--- linux/sendPacket.c
+++ linux/sendPacket.c
@@ -7,7 +7,11 @@
 #include "udp.h"
 #include "mfs.h"
 
-
+/**
+ * method that sends a message (through a packet).  Used by the client.
+ * The server will take the 'response' packet and fill in the important 
+ * bits of information, depending on the call (done in response packet)
+ */
 int sendPacket(char* hostname, int port_num, MFS_Packet_t *sent, MFS_Packet_t *response) {
   int sd = UDP_Open(0);
   if (sd < 0) {
@@ -15,6 +19,7 @@ int sendPacket(char* hostname, int port_num, MFS_Packet_t *sent, MFS_Packet_t *r
     return -1;
   }
 
+  //this code taken or slightly modified from original client
   struct sockaddr_in addr, addr2;
   int rc = UDP_FillSockAddr(&addr, hostname, port_num);
   if (rc < 0) {
@@ -22,6 +27,7 @@ int sendPacket(char* hostname, int port_num, MFS_Packet_t *sent, MFS_Packet_t *r
     return -1;
   }
 
+  //set up things for the select call (3 second timeout)
   fd_set read_fds;
   struct timeval tv;
   tv.tv_sec = 3;    //3 seconds exactly, 0 microseconds                             
@@ -31,27 +37,23 @@ int sendPacket(char* hostname, int port_num, MFS_Packet_t *sent, MFS_Packet_t *r
   while(1) {
     FD_ZERO(&read_fds);      //clear rfds to 0                              
     FD_SET(sd, &read_fds);   //adds the file descriptor to the set                  
+    
     // Send a packet to the server
     UDP_Write(sd, &addr, (char*)sent, sizeof(MFS_Packet_t));
 
     // Chillin waiting for a response (select described in P5 description)
-    if (select(sd + 1, &read_fds, NULL, NULL, &tv)) { // Select waits for an fd to become "ready"
+    if (select(sd + 1, &read_fds, NULL, NULL, &tv)) {   //waits for an fd to become "ready"
 
-      // Read a server response into "response packet" (I'm assuming this works
-      // correctly because tracing though all the calls is kind of a pain).
-      // You know what they say about assuming though...
+      // Read a server response into "response packet"
       rc = UDP_Read(sd, &addr2, (char*)response, sizeof(MFS_Packet_t)); 
-      if (rc >= 0) { // rc is the return of recvfrom (see man page)
-        
-        // Maybe want to check the response from the server here first?
+      
+      //rc is the return of recvfrom (see man page)
+      if (rc >= 0) { 
         UDP_Close(sd);
         return 0;
       }
-      else {
-	  //do something else  
-        fprintf(stderr, "Something! Most likely an error");
-      }
     } //end if(select())                                                            
+
   } //end while(1)                                                                  
 }
 
